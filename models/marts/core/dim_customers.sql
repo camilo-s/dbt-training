6a5d382f785f6a71cac22fe437470ec36c6951ca
csarmiento@thinkport.digital <csarmiento@thinkport.digital>
2023-06-26 08:30:09 +0000

    Add exemplar solution

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,41 +1,20 @@
 with customers as (
-
     select * from {{ ref('stg_customers')}}
-
 ),
-
 orders as (
-
-    select * from {{ ref('stg_orders') }}
-
+    select * from {{ ref('fct_orders')}}
 ),
-
-payments_orders as (
-
-    select * from {{ ref('fct_orders') }}
-
-),
-
 customer_orders as (
-
     select
-        orders.customer_id,
-
-        min(orders.order_date) as first_order_date,
-        max(orders.order_date) as most_recent_order_date,
-        count(orders.order_id) as number_of_orders,
-        sum(payments_orders.amount) as lifetime_value 
+        customer_id,
+        min(order_date) as first_order_date,
+        max(order_date) as most_recent_order_date,
+        count(order_id) as number_of_orders,
+        sum(amount) as lifetime_value
     from orders
-    where payments_orders.payment_status = 'success'
-    left join payments_orders using (order_id)
-
-
     group by 1
-
 ),
-
 final as (
-
     select
         customers.customer_id,
         customers.first_name,
@@ -44,11 +23,7 @@ final as (
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
         customer_orders.lifetime_value
-
     from customers
-
     left join customer_orders using (customer_id)
-
 )
-
 select * from final
